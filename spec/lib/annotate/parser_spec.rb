dazralsky/annotate_models

commit 5e3820b8f154950b6f74ec8f9ab5afc61836897e

annotate --routes modifies only routes.rb (#485)
* annotate --routes modifies only routes.rb

This change is a proposed solution to #357 and an alternative to the already proposed solution #361.
In #361 it is needed to call `annotate --routes --ignore-models` to achieve the same as in this change with only `annotate --routes`.

* Call eager_load only when models are included

This to prevent activerecord model errors when using mongoid for example and calling `annotate --routes`

lib/annotate.rb
@@ -114,7 +114,7 @@ module Annotate
   end
 
   def self.include_models?
-    true
+    ENV['routes'] !~ TRUE_RE
   end
 
   def self.loaded_tasks=(val)

bin/annotate
@@ -202,7 +202,7 @@ end.parse!
 options = Annotate.setup_options(
   is_rake: ENV['is_rake'] && !ENV['is_rake'].empty?
 )
-Annotate.eager_load(options)
+Annotate.eager_load(options) if Annotate.include_models?
 
 AnnotateModels.send(target_action, options) if Annotate.include_models?
 AnnotateRoutes.send(target_action, options) if Annotate.include_routes?

commit 4b388c1f04afd07a8349c8922d48feb1a0a0f55b

Make it possible to annotate models and routes together (#647)
Prior to this change, `Annotate.include_models?` returned the inverse of `Annotate.include_routes?`. This made it so annotating models and routes was not possible to do together.

This PR adds an explicit `--models` flag and also adds it the option to `lib/generators/annotate/templates/auto_annotate_models.rake` with the default being set to `false`. 

Fixes #563 and undoes the bug introduced in #485.

README.rdoc
@@ -89,11 +89,11 @@ To annotate all your models, tests, fixtures, and factories:
 
 To annotate just your models, tests, and factories:
 
-    annotate --exclude fixtures
+    annotate --models --exclude fixtures
 
 To annotate just your models:
 
-    annotate --exclude tests,fixtures,factories,serializers
+    annotate --models
 
 To annotate routes.rb:
 
@@ -184,6 +184,7 @@ you can do so with a simple environment variable, instead of editing the
             --wo, --wrapper-open STR     Annotation wrapper opening.
             --wc, --wrapper-close STR    Annotation wrapper closing
         -r, --routes                     Annotate routes.rb with the output of 'rake routes'
+            --models                     Annotate ActiveRecord models
         -a, --active-admin               Annotate active_admin models
         -v, --version                    Show the current version of this gem
         -m, --show-migration             Include the migration version number in the annotation

lib/annotate.rb
@@ -37,7 +37,7 @@ module Annotate
   ].freeze
   OTHER_OPTIONS = [
     :additional_file_patterns, :ignore_columns, :skip_on_db_migrate, :wrapper_open, :wrapper_close,
-    :wrapper, :routes, :hide_limit_column_types, :hide_default_column_types,
+    :wrapper, :routes, :models, :hide_limit_column_types, :hide_default_column_types,
     :ignore_routes, :active_admin
   ].freeze
   PATH_OPTIONS = [
@@ -115,7 +115,7 @@ module Annotate
   end
 
   def self.include_models?
-    ENV['routes'] !~ TRUE_RE
+    ENV['models'] =~ TRUE_RE
   end
 
   def self.loaded_tasks=(val)

lib/annotate/parser.rb
@@ -123,6 +123,10 @@ module Annotate
         env['routes'] = 'true'
       end
 
+      option_parser.on('--models', "Annotate routes.rb with the output of 'rake routes'") do
+        env['models'] = 'true'
+      end
+
       option_parser.on('-a', '--active-admin', 'Annotate active_admin models') do
         env['active_admin'] = 'true'
       end

lib/generators/annotate/templates/auto_annotate_models.rake
@@ -9,6 +9,7 @@ if Rails.env.development?
     Annotate.set_defaults(
       'additional_file_patterns'    => [],
       'routes'                      => 'false',
+      'models'                      => 'false',
       'position_in_routes'          => 'before',
       'position_in_class'           => 'before',
       'position_in_test'            => 'before',

spec/lib/annotate/parser_spec.rb
@@ -222,6 +222,17 @@ module Annotate # rubocop:disable Metrics/ModuleLength
       end
     end
 
+    %w[--models].each do |option|
+      describe option do
+        let(:env_key) { 'models' }
+        let(:set_value) { 'true' }
+        it 'sets the ENV variable' do
+          expect(ENV).to receive(:[]=).with(env_key, set_value)
+          Parser.parse([option])
+        end
+      end
+    end
+
     %w[-a --active-admin].each do |option|
       describe option do
         let(:env_key) { 'active_admin' }